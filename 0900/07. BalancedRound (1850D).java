a49a9dabcd43cf506bd593cf68fef2969cc679ee
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-05-16 18:36:38 +0530

    Add BalancedRound problem solution in Java

---
+++ 0900/07. BalancedRound (1850D).java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Main {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner sc  = new Scanner(System.in);
+        int testcases = sc.nextInt();
+        for(long t = 0;t<testcases; t++){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i=0;i<n;i++) arr[i] = sc.nextInt();
+            Arrays.sort(arr);
+            int count = 0;
+            int longest = 0;
+            for(int i = 1;i<n; i++){
+                if(arr[i]-arr[i-1]<=k){
+                    count++;
+                    longest = Math.max(longest,count);
+                }
+                else count = 0;
+            }
+            System.out.println(n-Math.max(longest,count)-1);
+        } 
+    }   
+}
+// k = 3
+// 1 3 5 12 12 17 17 20
+
+// c=3
+
+// 8 3 1 4 5 10 7 3
+
+// 1 2 3 4 6 7 8 9 11 12 13 14 15
+
+// 1 3 3 4 5 7 8 10
+
+// c = 1
+
