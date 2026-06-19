4fdf8a5bf91484d57a4c2ee11df98e3008ba4958
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-06-19 10:00:32 +0530

    Add Main class with input handling and output

---
+++ 0900/25. BadBoy (1537B).java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+
+        while (tc-- > 0) {
+           int m = sc.nextInt(); int n = sc.nextInt();
+           sc.nextInt();
+           sc.nextInt();
+           System.out.println(m+" "+n+ " " + 1+ " " + 1);
+        }
+    }
+}
