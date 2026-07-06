626ea9ec04ec454eec4dcb5d371c234379b0cbc6
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-07-06 10:16:31 +0530

    Implement Olya's Game with Arrays solution

---
+++ 1000/04. Olya and Game with Arrays(1859B).java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0) {
+			int n = sc.nextInt();
+			long  minGain = Long.MAX_VALUE;
+			long  sbkaMin = Long.MAX_VALUE;
+			long  sbkaSecMin = Long.MAX_VALUE;
+			long sum = 0;
+			for(int i=0; i<n; i++) {
+				int m = sc.nextInt();
+				long min =  Long.MAX_VALUE;
+				long smin = Long.MAX_VALUE;
+				for(int j=0; j<m; j++) {
+					long curr = sc.nextInt();
+					if(curr < min) {
+						smin = min;
+						min = curr;
+					} else if(curr<smin) smin = curr;
+				}
+				sbkaMin = Math.min(sbkaMin, min);
+				sbkaSecMin = Math.min(sbkaSecMin, smin);
+				sum += smin;
+			}
+			// ek array esi hogi, jisme secMin sbse chhota hoga, uske secMin nhi lege, usko fook aur usme global min daaldege
+			sum -= sbkaSecMin;
+			sum += sbkaMin;
+			System.out.println(sum);
+		}
+	}
+}
