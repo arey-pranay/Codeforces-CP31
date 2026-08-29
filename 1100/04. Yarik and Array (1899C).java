e331977024bfe5c6c0e3d4d083f7172ca18e319c
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-08-29 12:31:43 +0530

    Update 04. Yarik and Array (1899C).java

--- 1100/04. Yarik and Array (1899C).java
+++ 1100/04. Yarik and Array (1899C).java
@@ -3,14 +3,14 @@ public class Main{
     public static void main(String[] args) {
       Scanner sc  = new Scanner(System.in);
       int t = sc.nextInt();
-      while(t-->0){       
-          int ans = Integer.MIN_VALUE;
+      while(t-->0){  
           int n = sc.nextInt();
-          int prev = sc.nextInt();
+           int prev = sc.nextInt();
+            int ans = prev;
           int sum = prev;
           for(int i=1;i<n;i++){
-             num = sc.nextInt();
-             if(prev%2==0 != num%2==0) sum = Math.max(sum,sum+num);
+             int num = sc.nextInt();
+             if((prev%2==0) != (num%2==0)) sum = Math.max(num,sum+num);
              else sum=num;
              ans = Math.max(ans,sum);
              prev = num;
@@ -19,5 +19,4 @@ public class Main{
       }
     }
 }
-
-//Kadane's Algo with parity condition check
+//Kadane's algorithm with parity check condition
